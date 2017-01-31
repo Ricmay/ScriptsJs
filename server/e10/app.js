63e7cbc3a4a1368508eeb230049f397946d7b77e
Ricmay <jrmc_95@hotmail.com>
2017-01-30 19:13:55 -0500

    Add files via upload

---
+++ server/e10/app.js
@@ -0,0 +1,17 @@
+'use strict';
+'use strict';
+angular.module('myApp', [
+'myApp.controllers',
+'ngRoute'
+]);
+angular.module('myApp').config(function($routeProvider,
+➥$locationProvider){
+$routeProvider.when('/view1',{
+controller:'Controller1',
+templateUrl:'/partials/view1.html'
+}).when('/view2/:firstname/:lastname',{
+controller: 'Controller2',
+templateUrl: '/partials/view2.html'
+}).otherwise({redirectTo:'/view1'});
+$locationProvider.html5Mode(true);
+});
